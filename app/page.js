71c0860f8fdd75478601ff66abc884c2b814fccd
MALIKKHUBAIB1 <khubaib9221@gmail.com>
2024-10-14 22:51:11 +0530

    installing the database and fixing some bugs

--- app/page.js
+++ app/page.js
@@ -1,11 +1,9 @@
-import { Accordion } from "@/components/ui/accordion";
-import { Button } from "@/components/ui/button";
+import { Button } from "../components/ui/button";
 
 export default function Home() {
   return (
     <div>
       <h2>Mocak Mate App</h2>
-      <Accordion />
       <Button>Shadcn</Button>
     </div>
   );
